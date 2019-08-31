43e8518bc577f76077487dcee520e3f571a2c8c6
Jul22 <gerasimovayuliya88@gmail.com>
2019-08-31 11:50:41 +0300

    добавляет графику

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,4 @@
+//menu://
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
@@ -11,4 +12,30 @@ navToggle.addEventListener('click', function() {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
   }
+});
+
+//form://
+
+var surName = document.querySelector('#sur-name');
+var Name = document.querySelector('#name');
+var email = document.querySelector('#mail');
+var submit = document.querySelector('.contest-form__submit-button');
+var Success = document.querySelector('.popup-success');
+var Error = document.querySelector('.popup-error');
+var Close = document.querySelector('.popup__button');
+
+submit.addEventListener("click", function(event){
+  event.preventDefault();
+  if (surName.value && Name.value && email.value){
+    Success.classList.add('popup-success--alert');
+  }
+  else{
+    Error.classList.add('popup-error--alert');
+  }
+});
+
+Close.addEventListener("click", function(event){
+  event.preventDefault();
+  Success.classList.remove('popup-success--alert');
+  Error.classList.remove('popup-error--alert');
 });
